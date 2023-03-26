2526747718ce0e4f0dd8fa1ea1d922f540cffce9
Pablo Montepagano <pablo@montepagano.com.ar>
2023-03-26 13:35:14 -0300

    CFSM parser tests.

--- internal/contract/contract_test.go
+++ internal/contract/contract_test.go
@@ -6,7 +6,8 @@ import (
 )
 
 func TestHelloWorldFSAParse(t *testing.T) {
-	const exampleFSAContent = `.outputs A
+	const exampleFSAContent = `
+.outputs A
 .state graph
 q0 1 ! hello q1
 q0 1 ! world q1
@@ -46,6 +47,60 @@ q0 0 ? world q1
 	}
 	if firstCFSM.Start.Label != "q0" {
 		t.Errorf("Expected start state of first CFSM to be 'q0'. Found %s instead", firstCFSM.Start.Label)
+	}
+
+}
+
+func TestPingPongFSAParser(t *testing.T) {
+	const exampleFSAContent = `
+.outputs Ping
+.state graph
+0 1 ! ping 5
+2 1 ? bye 1
+3 1 ! bye 2
+3 1 ! finished 2
+4 1 ! *<1 0
+4 1 ! >*1 3
+5 1 ? pong 4
+.marking 0
+.end
+
+.outputs Pong
+.state graph
+0 0 ? ping 5
+2 0 ! bye 1
+3 0 ? bye 2
+3 0 ? finished 2
+4 0 ? *<1 0
+4 0 ? >*1 3
+5 0 ! pong 4
+.marking 0
+.end
+`
+	fs := fstest.MapFS{
+		"example.fsa": {Data: []byte(exampleFSAContent)},
+	}
+
+	exampleFile, err := fs.Open("example.fsa")
+	if err != nil {
+		t.Error("Failed reading example.fsa.")
+	}
+	defer exampleFile.Close()
+
+	sys, err := ParseFSAFile(exampleFile)
+	if err != nil {
+		t.Errorf("Error parsing FSA: %s", err)
+	}
+
+	if len(sys.CFSMs) != 2 {
+		t.Errorf("Parsed FSA has %d CFSMs. Expected 2.", len(sys.CFSMs))
+	}
+	firstCFSM := sys.CFSMs[0]
+	if firstCFSM.Comment != "Ping" {
+		t.Errorf("Expected fist CFSM to have Comment 'Ping' but instead had %s", firstCFSM.Comment)
+	}
+	if len(firstCFSM.States()) != 6 {
+		t.Errorf("Expected 6 states in first CFSM. Found %d", len(firstCFSM.States()))
 	}
 
 }
