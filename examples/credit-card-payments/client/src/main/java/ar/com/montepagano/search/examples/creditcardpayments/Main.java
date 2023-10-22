a3b9fdc876989f89abd2742a8e4aa572e1a6794b
Pablo Montepagano <pablo@montepagano.com.ar>
2023-10-22 19:24:33 -0300

    WIP: proyecto de cliente en Java.

--- examples/credit-card-payments/client/src/main/java/ar/com/montepagano/search/examples/creditcardpayments/Main.java
+++ examples/credit-card-payments/client/src/main/java/ar/com/montepagano/search/examples/creditcardpayments/Main.java
@@ -1,26 +1,40 @@
+package ar.com.montepagano.search.examples.creditcardpayments;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.FileInputStream;
+import com.google.protobuf.ByteString;
 import ar.com.montepagano.search.middleware.v1.PrivateMiddlewareServiceGrpc;
 import ar.com.montepagano.search.middleware.v1.Middleware.RegisterChannelRequest;
 import ar.com.montepagano.search.middleware.v1.Middleware.RegisterChannelResponse;
+import ar.com.montepagano.search.contracts.v1.Contracts.GlobalContract;
 
-public class Client {
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
     public static void main(String[] args) {
+        System.out.println( "Hello World!" );
         Scanner scanner = new Scanner(System.in);
 
         // List of book titles
         String[] bookTitles = {
-            "The Great Gatsby",
-            "To Kill a Mockingbird",
-            "1984",
-            "Pride and Prejudice",
-            "Animal Farm",
-            "The Catcher in the Rye",
-            "Lord of the Flies",
-            "Brave New World",
-            "The Hobbit",
-            "The Fellowship of the Ring"
+                "The Great Gatsby",
+                "To Kill a Mockingbird",
+                "1984",
+                "Pride and Prejudice",
+                "Animal Farm",
+                "The Catcher in the Rye",
+                "Lord of the Flies",
+                "Brave New World",
+                "The Hobbit",
+                "The Fellowship of the Ring"
         };
 
         // Prompt user to select book titles
@@ -50,10 +64,19 @@ public class Client {
         }
         System.out.println("Shipping Address: " + shippingAddress);
 
+        // Load file contract.fsa into a ByteString variable
+        ByteString contractBytes = null;
+        try {
+            contractBytes = ByteString.readFrom(new FileInputStream("./contract.fsa"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        GlobalContract contract = GlobalContract.newBuilder().setContract(contractBytes).build();
+
         // Register channel with middleware
         ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:1234").usePlaintext().build();
         PrivateMiddlewareServiceGrpc.PrivateMiddlewareServiceBlockingStub stub = PrivateMiddlewareServiceGrpc.newBlockingStub(channel);
-        RegisterChannelRequest request = RegisterChannelRequest.newBuilder().build(); // Fill in the contract
+        RegisterChannelRequest request = RegisterChannelRequest.newBuilder().setRequirementsContract(contract).build();
         RegisterChannelResponse response = stub.registerChannel(request);
         channel.shutdown();
     }
